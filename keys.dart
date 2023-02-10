39a9c8af731db53f1b4267284373c8ad5e826d47
ZEYAD-8 <104276842+ZEYAD-8@users.noreply.github.com>
2023-02-10 22:34:29 +0200

    Update keys.dart

--- keys.dart
+++ keys.dart
@@ -1,6 +1,6 @@
 void main(List<String> args) {
   // Creating a funny map and a list for the keys
-  Map ThisIsAMap = {'Name' : 'Dora', 'Age' : 'Undefined' , 'Job' : "Can't find one", 'Address' : 'Homeless'};
+  Map ThisIsAMap = {'Name' : 'Dora', 'Age' : '9 years old' , 'Job' : "Can't find a job", 'Address' : 'Homeless'};
   List keysOfthatMap = [];
 
   // Iterating over the map and saving the keys in the list
