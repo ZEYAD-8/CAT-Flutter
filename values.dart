b13eeb39817b21a7c9a75a376d8d295b976013f9
ZEYAD-8 <104276842+ZEYAD-8@users.noreply.github.com>
2023-02-10 22:48:40 +0200

    Update values.dart

--- values.dart
+++ values.dart
@@ -1,13 +1,24 @@
 void main(List<String> args) {
   // Creating a funny map and a list for the values
   Map ThisIsAMap = {'Name' : 'Dora', 'Age' : '9 years old' , 'Job' : "Can't find a job", 'Address' : 'Homeless'};
-  List valuesOfthatMap = [];
+
+  // Calling the function to get the values
+  List valuesOfthatMap = getValues(ThisIsAMap);
+
+  // Printing the values
+  print(valuesOfthatMap);
+}
+
+// This function takes a Map and return the values
+List getValues (Map ThisIsAMap)
+{
+  // Creating a list to save the values
+  List valuesOftheMap = [];
 
   // Iterating over the map and saving the saving in the list
   ThisIsAMap.forEach((key, value) { 
-    valuesOfthatMap.add(value);
+    valuesOftheMap.add(value);
   });
 
-  //Printing the keys
-  print(valuesOfthatMap);
+  return valuesOftheMap;
 }
